8a77c16789b1725cd80b7cf988cb60ca009836af
Elisa200307 <elisarenzetti1@gmail.com>
2025-11-03 16:37:11 +0100

    aggiunto da garacavalli

--- garacavalli/src/main/java/com/mycompany/garacavalli/Main.java
+++ garacavalli/src/main/java/com/mycompany/garacavalli/Main.java
@@ -3,49 +3,69 @@
  */
 package com.mycompany.garacavalli;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.lang.Math.*;
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
 
-/**
- *
- * @author elisarenzetti
- */
+public class Main {
 
+    public static void main(String[] args) {
+        Scanner tastiera = new Scanner(System.in);
 
-    public class Main {
+        System.out.print("Inserisci la lunghezza del percorso (in metri): ");
+        int percorso = tastiera.nextInt();
 
-        public static void main(String[] args) {
-            Scanner tastiera = new Scanner(System.in);
+        ArrayList<String> classifica = new ArrayList<>();
+        ArrayList<Cavallo> cavalli = new ArrayList<>();
 
-            System.out.println("Inserisci la lunghezza del percorso (in metri): ");
-            int percorso = tastiera.nextInt();
+        for (int i = 1; i <= 5; i++) {
+            cavalli.add(new Cavallo("Cavallo" + i, percorso, classifica));
+        }
 
-            ArrayList<String> classifica = new ArrayList<String>();
-            ArrayList<Cavallo> cavalli = new ArrayList<Cavallo>();
+        cavalli.forEach(Thread::start);
 
-            for (int i = 0; i < 5; i++) {
-                cavalli.add(new Cavallo("cavallo" + i, percorso, classifica));
+        for (Cavallo c : cavalli) {
+            try {
+                c.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+        }
 
-            cavalli.forEach(cavallo -> {
-                cavallo.run();
-            });
-
-            cavalli.forEach(cavallo -> {
-                try {
-                    cavallo.join();
-                } catch (InterruptedException e) {
-                    System.out.println("stacktrace:\n");
-                    e.printStackTrace();
-                }
-            });
-
-            System.out.println("\n CLASSIFICA ");
-            for (int i = 0; i < classifica.size(); i++) {
+        System.out.println("\nCLASSIFICA FINALE");
+        for (int i = 0; i < classifica.size(); i++) {
+            if (i == 0) {
+                System.out.println((i + 1) + "° posto: " + classifica.get(i) + " (vincitore)");
+            } else {
                 System.out.println((i + 1) + "° posto: " + classifica.get(i));
             }
         }
+
+        salvaSuFile(classifica);
     }
 
+    private static void salvaSuFile(List<String> classifica) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Scegli dove salvare la classifica");
+
+        int scelta = fileChooser.showSaveDialog(null);
+        if (scelta == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                writer.write("CLASSIFICA GARA CAVALLI\n\n");
+                for (int i = 0; i < classifica.size(); i++) {
+                    if (i == 0)
+                        writer.write((i + 1) + "° posto: " + classifica.get(i) + " (vincitore)\n");
+                    else
+                        writer.write((i + 1) + "° posto: " + classifica.get(i) + "\n");
+                }
+                System.out.println("\nClassifica salvata nel file: " + file.getAbsolutePath());
+            } catch (IOException e) {
+                System.out.println("Errore nel salvataggio del file: " + e.getMessage());
+            }
+        } else {
+            System.out.println("Salvataggio annullato.");
+        }
+    }
+}
