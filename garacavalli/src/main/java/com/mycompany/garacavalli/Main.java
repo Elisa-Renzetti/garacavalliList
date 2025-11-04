0e8ac76befae7f21bbbb4a2f5947638ca99f82b7
Elisa-Renzetti <renzettielisa3@gmail.com>
2025-11-04 10:43:40 +0100

    Update Main.java

--- garacavalli/src/main/java/com/mycompany/garacavalli/Main.java
+++ garacavalli/src/main/java/com/mycompany/garacavalli/Main.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void Gestoregaracavalli(String[] args) {
         Scanner tastiera = new Scanner(System.in);
 
         System.out.print("Inserisci la lunghezza del percorso (in metri): ");
